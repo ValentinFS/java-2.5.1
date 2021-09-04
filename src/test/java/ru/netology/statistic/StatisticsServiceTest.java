83344e7c3f1ced056e8a69cbab68702f497482d5
Valentin Fadeev <valentinfs@yandex.ru>
2021-09-04 18:29:46 +0300

    StatisticsServiceTest updated

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,11 +10,17 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions={12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
-        long actual=service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
+
+        long[] incomesInBillionsDifferent = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long actualDifferent = service.findMax(incomesInBillionsDifferent);
+        assertEquals(expected, actualDifferent);
+
+
     }
+
 
 }
